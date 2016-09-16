904b7b4f85d287836edc56b6fc4134b01a0865cf
Jilles van Gurp <jilles@jillesvangurp.com>
2016-09-16 18:40:26 +0200

    add a simple trie implementation

---
+++ src/main/java/io/inbot/utils/SimpleStringTrie.java
@@ -0,0 +1,88 @@
+package io.inbot.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Simple implementation of a Trie that may be used to match input strings to the longest matching prefix.
+ */
+public class SimpleStringTrie {
+    private final TrieNode root;
+
+    private static class TrieNode {
+        private final Map<Character, TrieNode> children;
+        boolean end=false;
+
+        public TrieNode(char ch) {
+            children = new HashMap<>();
+        }
+
+        public Map<Character, TrieNode> getChildren() {
+            return children;
+        }
+
+        public boolean isLeaf() {
+            return end;
+        }
+    }
+
+    public SimpleStringTrie() {
+        root = new TrieNode((char) 0);
+    }
+
+    /**
+     * Useful if you want to build a trie for an existing map so you can figure out a matching prefix that has an entry
+     */
+    public static SimpleStringTrie from(Map<String,?> map) {
+        SimpleStringTrie st = new SimpleStringTrie();
+        map.keySet().forEach(key -> st.add(key));
+        return st;
+    }
+
+    /**
+     * Add a string to the trie.
+     * @param input any String
+     */
+    public void add(String input) {
+        TrieNode currentNode = root;
+
+        for(char c: input.toCharArray()) {
+            Map<Character, TrieNode> children = currentNode.getChildren();
+            TrieNode matchingNode = children.get(c);
+            if(matchingNode != null) {
+                currentNode = matchingNode;
+            } else {
+                TrieNode newNode = new TrieNode(c);
+                children.put(c, newNode);
+                currentNode = newNode;
+            }
+        }
+        currentNode.end=true;
+    }
+
+    /**
+     * Return the longest matching prefix of the input string that was added to the trie.
+     * @param input a string
+     * @return Optional of longest matching prefix that was added to the trie
+     */
+    public Optional<String> get(String input) {
+        TrieNode currentNode = root;
+        int i=0;
+        for(char c: input.toCharArray()) {
+            TrieNode nextNode = currentNode.getChildren().get(c);
+            if(nextNode != null) {
+                i++;
+                currentNode=nextNode;
+            } else {
+                if(i>0 && currentNode.isLeaf()) {
+                    return Optional.of(input.substring(0, i));
+                }
+            }
+        }
+        if(i>0 && currentNode.isLeaf()) {
+            return Optional.of(input.substring(0, i));
+        }
+        return Optional.empty();
+   }
+}
---
+++ src/test/java/io/inbot/utils/SimpleStringTrieTest.java
@@ -0,0 +1,31 @@
+package io.inbot.utils;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+import org.testng.annotations.Test;
+
+@Test
+public class SimpleStringTrieTest {
+    public void shouldTriePrefixes() {
+        SimpleStringTrie trie = new SimpleStringTrie();
+        // these prefixes we want to find
+        trie.add("foofo");
+        trie.add("foofoo");
+        trie.add("fooboooooo");
+        trie.add("1234");
+        trie.add("123");
+        // these inputs have no prefix that was added
+        assertThat(trie.get("foo").isPresent()).isFalse();
+        assertThat(trie.get("foobar").isPresent()).isFalse();
+        assertThat(trie.get("abc").isPresent()).isFalse();
+        assertThat(trie.get("1200").isPresent()).isFalse();
+        // these inputs have matching prefixes that were added
+        assertThat(trie.get("foofo").get()).isEqualTo("foofo");
+        assertThat(trie.get("foofoxxxxxxxxxx").get()).isEqualTo("foofo");
+        assertThat(trie.get("foofoo").get()).isEqualTo("foofoo");
+        assertThat(trie.get("foofoooo").get()).isEqualTo("foofoo");
+        assertThat(trie.get("fooboooooo").get()).isEqualTo("fooboooooo");
+        assertThat(trie.get("1234").get()).isEqualTo("1234");
+        assertThat(trie.get("1230").get()).isEqualTo("123");
+    }
+}
