c9280a83b9be7db5a955aebbaf75157d909c32d4
Jilles van Gurp <jilles@jillesvangurp.com>
2017-06-06 17:08:19 +0200

    add streamMatches

--- src/main/java/io/inbot/utils/MiscUtils.java
+++ src/main/java/io/inbot/utils/MiscUtils.java
@@ -4,6 +4,13 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.function.Consumer;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class MiscUtils {
 
@@ -40,5 +47,23 @@ public class MiscUtils {
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException("get a jdk that actually supports utf-8", e);
         }
+    }
+
+    public static Stream<MatchResult> streamMatches(Matcher matcher) {
+        // thank you SO: https://stackoverflow.com/questions/28148483/how-do-i-create-a-stream-of-regex-matches
+
+        Spliterator<MatchResult> spliterator = new Spliterators.AbstractSpliterator<MatchResult>(Long.MAX_VALUE, Spliterator.ORDERED|Spliterator.NONNULL) {
+
+            @Override
+            public boolean tryAdvance(Consumer<? super MatchResult> action) {
+                if(!matcher.find()) {
+                    return false;
+                } else {
+                    action.accept(matcher.toMatchResult());
+                    return true;
+                }
+            }};
+
+        return StreamSupport.stream(spliterator, false);
     }
 }
